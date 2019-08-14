12ea4c811dd99b10d5e645a92bfd31ce6e26a2ae
Petru Ritivoiu <p.ritivoiu@gmail.com>
2019-08-14 13:19:56 +0300

    cleanup + default interface methods

---
+++ CSharpLang8/AsyncStreams/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncStreams
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Async Streams");
+
+            await foreach (var current in GetData())
+            {
+                Console.WriteLine(current);
+            }
+
+            #region Lyrics
+            await foreach (var current in GetLyrics())
+            {
+                Console.WriteLine(current);
+            }
+            #endregion
+        }
+
+        #region Data
+        public static async IAsyncEnumerable<int> GetData()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                yield return await Task.Delay(500).ContinueWith((_) => i);
+            }
+        }
+
+        public static async IAsyncEnumerable<string> GetLyrics()
+        {
+            using var sr = new StreamReader("Lyrics.txt");
+            string line = string.Empty;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                yield return await Task.Delay(1000).ContinueWith((_) => line);
+            }
+        }
+        #endregion
+    }
+
+}
---
+++ CSharpLang8/DefaultInterfaceMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DefaultInterfaceMethods
+{
+    interface IRepository
+    {
+        public string Get();
+
+        public string About() => "Default implementation of About method";
+
+        //Having a default implementation for a method does NOT enforce classes
+        //that implement this interface to provide an implementation (override)
+        //for this method
+        public string Describe() => "Default implementation of Describe method";
+
+    }
+
+    public class Repository : IRepository
+    {
+        public string Get() => "Return some data";
+
+        //Overriding About method is possible but not mandatory
+        public string About() => "Override About method";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repository = new Repository() as IRepository;
+
+            Console.WriteLine(repository.About()); // => Override About method
+
+            Console.WriteLine(repository.Describe()); // => Default implementation of Describe method
+        }
+    }
+}
